ec9e9fb4f074e8a3f57952061589b00f87b8ea2c
Joel Hagan <joel.hagan.8@gmail.com>
2023-01-27 16:27:52 -0700

    add dynamic AJAX request and formatting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,44 +31,49 @@ $(document).ready(function() {
       }
     ]
 
-    const renderTweets = function(tweets) {
-      for (let tweet of tweets) {
-        const $tweet = createTweetElement(tweet);
-        console.log($tweet);
-          $('#tweets-container').append($tweet);
-      }
-    }
-
     const createTweetElement = function(tweetData) {
       let $tweet = $(`
         <article class="tweets-container">
-        <header class="tweets-header">
-          <div>
-            <i class="fa-solid fa-face-grin-beam src="${tweetData.user.avatars}"></i>
-            <span>${tweetData.user.name}</span>
-          </div>
-          <div class="tweets-handler" src="${tweetData.user.handle}>
-            <span>@JHagan</span>
-          </div>
-        </header>
-        <main class="tweets-main">
-          <span>${tweetData.content.text}</span>
-        </main>
-        <footer class="tweets-footer">
-          <span>${tweetData.created_at}</span>
-            <div class="tweets-icons">
-              <span>
-                <i class="fa-solid fa-flag"></i>
-                <i class="fa-solid fa-retweet"></i>
-                <i class="fa-solid fa-heart"></i>
-              </span>
+          <header class="tweets-header">
+            <div>
+              <img src="${tweetData.user.avatars}"/>
+              <span>${tweetData.user.name}</span>
             </div>
-        </footer>
-      </article>
+            <div class="tweets-handler">
+              <span>${tweetData.user.handle}</span>
+            </div>
+          </header>
+          <main class="tweets-main">
+            <span>${tweetData.content.text}</span>
+          </main>
+          <footer class="tweets-footer">
+            <span>${tweetData.created_at}</span>
+              <div class="tweets-icons">
+                <span>
+                  <i class="fa-solid fa-flag"></i>
+                  <i class="fa-solid fa-retweet"></i>
+                  <i class="fa-solid fa-heart"></i>
+                </span>
+              </div>
+          </footer>
+        </article>
       `)
       return $tweet
     }
 
-  renderTweets(dataFormat);
+    const renderTweets = function(tweets) {
+      for (let tweet of tweets) {
+        const $tweet = createTweetElement(tweet);
+          $('.tweets-border').append($tweet);
+      }
+    }
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      const newTweet = $('form').serialize();
+      $.post('/tweets', newTweet);
+    })
+
+    renderTweets(dataFormat);
 
 });
